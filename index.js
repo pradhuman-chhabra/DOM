ac6144618db7dc7d784181a14964cd7a48bc362f
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-08-15 16:49:27 +0530

    140. Adding Javascript to Websites

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("hello World");
+
+document.querySelector("h1").innerHTML="Good bye world";
+
+// this will get the element from the HTML using query selector and change the inner HTML to the specified string
